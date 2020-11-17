aee86c4a89b1d89fa91891aefa20fa7cd9c06230
Miotis2004 <miotis2004@gmail.com>
2020-11-17 12:09:23 -0500

    First Commit

---
+++ NewsOrg/NewsOrg/NewsOrgApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsOrgApp.swift
+//  NewsOrg
+//
+//  Created by Field Employee on 11/17/20.
+//
+
+import SwiftUI
+
+@main
+struct NewsOrgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
